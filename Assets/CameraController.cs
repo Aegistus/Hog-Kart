1454aa8d40084a49688d5549c8e805c2cae0e791
Joe Ballanderson <57004318+Aegistus@users.noreply.github.com>
2025-10-18 14:46:56 -0400

    Initial Commit

---
+++ Assets/CameraController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] Transform targetTransform;
+    [SerializeField] float smoothSpeed = 5f;
+    [SerializeField] float rotationSpeed = 5f;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        transform.Rotate(rotationSpeed * Input.GetAxis("Mouse X") * Time.deltaTime * Vector3.up);
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, targetTransform.position, smoothSpeed * Time.deltaTime);
+    }
+}
