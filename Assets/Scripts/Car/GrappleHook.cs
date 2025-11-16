25bb86e0b33020d112ae418eaad5f05ab14188ac
Joe Ballanderson <57004318+Aegistus@users.noreply.github.com>
2025-11-16 17:14:01 -0500

    Add Grapple Hook Animation

--- Assets/Scripts/Car/GrappleHook.cs
+++ Assets/Scripts/Car/GrappleHook.cs
@@ -5,15 +5,24 @@ using UnityEngine;
 public class GrappleHook : MonoBehaviour
 {
     [SerializeField] Transform grappleHookBarrel;
+    [SerializeField] Transform[] sensorPoints;
     [SerializeField] KeyCode inputKey;
     [SerializeField] float torqueConstant = 10000;
-    [SerializeField] Transform[] sensorPoints;
+    [SerializeField] int quality = 500;
+    [SerializeField] float damper = 14;
+    [SerializeField] float strength = 800;
+    [SerializeField] float velocity = 15;
+    [SerializeField] int waveCount = 4;
+    [SerializeField] float waveHeight = 1f;
+    [SerializeField] AnimationCurve effectCurve;
 
     public bool Grappling => grappling;
 
     Vector3 grapplePoint;
+    Vector3 currentRopePoint;
     Rigidbody rb;
     LineRenderer lineRend;
+    Spring spring;
     float radius;
     float radiusModifier = 1.2f;
     bool grappling = false;
@@ -23,6 +32,8 @@ public class GrappleHook : MonoBehaviour
     {
         rb = GetComponentInParent<Rigidbody>();
         lineRend = GetComponent<LineRenderer>();
+        spring = new Spring();
+        spring.SetTarget(0);
     }
 
     private void Update()
@@ -34,6 +45,7 @@ public class GrappleHook : MonoBehaviour
                 if (Physics.Raycast(sensor.position, sensor.forward, out RaycastHit rayHit, maxGrappleDistance))
                 {
                     grapplePoint = rayHit.point;
+                    currentRopePoint = grappleHookBarrel.position;
                     radius = Vector3.Distance(transform.position, grapplePoint) * radiusModifier;
                     grappling = true;
                     break;
@@ -43,8 +55,8 @@ public class GrappleHook : MonoBehaviour
         if (Input.GetKeyUp(inputKey))
         {
             grappling = false;
-            lineRend.SetPosition(0, grappleHookBarrel.position);
-            lineRend.SetPosition(1, grappleHookBarrel.position);
+            grapplePoint = grappleHookBarrel.position;
+            currentRopePoint = grappleHookBarrel.position;
         }
     }
 
@@ -63,11 +75,41 @@ public class GrappleHook : MonoBehaviour
 
     private void LateUpdate()
     {
-        if (Grappling)
+        DrawRope();
+    }
+
+    void DrawRope()
+    {
+        if (!Grappling)
         {
-            // visuals
-            lineRend.SetPosition(0, grapplePoint);
-            lineRend.SetPosition(1, grappleHookBarrel.position);
+            spring.Reset();
+            if (lineRend.positionCount > 0)
+            {
+                lineRend.positionCount = 0;
+            }
         }
+        else
+        {
+            if (lineRend.positionCount == 0)
+            {
+                spring.SetVelocity(velocity);
+                lineRend.positionCount = quality + 1;
+            }
+
+            spring.SetDamper(damper);
+            spring.SetStrength(strength);
+            spring.Update(Time.deltaTime);
+
+            var upVector = Quaternion.LookRotation((grapplePoint - grappleHookBarrel.position).normalized) * Vector3.up;
+            currentRopePoint = Vector3.Lerp(currentRopePoint, grapplePoint, Time.deltaTime * velocity);
+
+            for (int i = 0; i < quality + 1; i++)
+            {
+                var delta = i / (float)quality;
+                var offset = effectCurve.Evaluate(delta) * Mathf.Sin(delta * waveCount * Mathf.PI * spring.Value) * waveHeight * upVector;
+                lineRend.SetPosition(i, Vector3.Lerp(grappleHookBarrel.position, currentRopePoint, delta) + offset);
+            }
+        }
+
     }
 }
